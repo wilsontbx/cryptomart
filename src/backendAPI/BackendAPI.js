44cb8c7c11113e06b5c6eea8040ed818eb98da53
wilsontbx <boonxian1993@hotmail.com>
2021-02-16 15:20:53 +0800

    backend api is ready

--- src/backendAPI/BackendAPI.js
+++ src/backendAPI/BackendAPI.js
@@ -1,13 +1,17 @@
 import axios from "axios";
-import qs from "qs";
+// import qs from "qs";
 
-const baseUrl = "";
+const baseUrl = "http://localhost:5000/";
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
   timeout: 5000,
 });
 
-const backendAPI = {};
+const backendAPI = {
+  render: () => {
+    return axiosInstance.get("render");
+  },
+};
 
 export default backendAPI;
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,15 @@
 import React from "react";
+import backendAPI from "../backendAPI/BackendAPI";
 
 function MainPage() {
+  backendAPI
+    .render()
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   return <div>Hello World</div>;
 }
 
