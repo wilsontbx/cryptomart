cd9c2203d16e3d05bbdd13d9d47a7c4bb5bfa954
wilsontbx <boonxian1993@hotmail.com>
2021-02-18 15:33:38 +0800

    added logo

--- src/components/CryptoTable.js
+++ src/components/CryptoTable.js
@@ -21,26 +21,6 @@ import Switch from "@material-ui/core/Switch";
 import DeleteIcon from "@material-ui/icons/Delete";
 import FilterListIcon from "@material-ui/icons/FilterList";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
-
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -242,6 +222,11 @@ const useStyles = makeStyles((theme) => ({
     top: 20,
     width: 1,
   },
+  logo: {
+    height: "24px",
+    width: "24px",
+    borderRadius: "12px",
+  },
 }));
 
 export default function CryptoTable(props) {
@@ -262,7 +247,7 @@ export default function CryptoTable(props) {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
+      const newSelecteds = data.map((n) => n.name);
       setSelected(newSelecteds);
       return;
     }
@@ -304,9 +289,6 @@ export default function CryptoTable(props) {
 
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
@@ -331,7 +313,7 @@ export default function CryptoTable(props) {
               {stableSort(data, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
+                  const isItemSelected = isSelected(row.cmc_rank);
                   const labelId = `enhanced-table-checkbox-${index}`;
 
                   return (
@@ -358,7 +340,14 @@ export default function CryptoTable(props) {
                       >
                         {row.cmc_rank}
                       </TableCell>
-                      <TableCell align="left">{row.name}</TableCell>
+                      <TableCell align="left">
+                        <img
+                          className={classes.logo}
+                          src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${row.cmc_rank}.png`}
+                          alt={row.cmc_rank}
+                        />
+                        {row.name}
+                      </TableCell>
                       <TableCell align="right">{row.quote.USD.price}</TableCell>
                       <TableCell align="right">
                         {row.quote.USD.percent_change_24h}
@@ -384,7 +373,7 @@ export default function CryptoTable(props) {
         <TablePagination
           rowsPerPageOptions={[25, 50, 100]}
           component="div"
-          count={rows.length}
+          count={data.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onChangePage={handleChangePage}
