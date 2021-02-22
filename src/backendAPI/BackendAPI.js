09b4e383c85521e008620157501d1e79fc162daa
wilsontbx <boonxian1993@hotmail.com>
2021-02-22 14:00:14 +0800

    change prod url

--- src/backendAPI/BackendAPI.js
+++ src/backendAPI/BackendAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import qs from "qs";
 
 const devurl = "http://localhost:5000";
-const produrl = "https://express-cryptomart.netlify.app/";
+const produrl = "https://express-cryptomart.herokuapp.com/";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === "development" ? devurl : produrl,
